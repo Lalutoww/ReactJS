6b6082aa6302469d8c5b160d063566845ebeb763
Martin Lalutov <martinlalutov00@gmail.com>
2023-11-22 18:40:25 +0200

    turn register into controlled form

--- games-play/client/src/components/register/Register.jsx
+++ games-play/client/src/components/register/Register.jsx
@@ -1,27 +1,65 @@
+import useForm from '../../hooks/useForm.js';
+import { useContext } from 'react';
+import AuthContext from '../../contexts/authContext.js';
+
+const RegisterFormKeys = {
+   Email: 'email',
+   Password: 'password',
+   ConfirmPass: 'confirm-password',
+};
+
 export default function Register() {
-    return (
-        <section id="register-page" className="content auth">
-            <form id="register">
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Register</h1>
+   const { registerSubmitHandler } = useContext(AuthContext);
+   const { values, onChange, onSubmit } = useForm(registerSubmitHandler, {
+      [RegisterFormKeys.Email]: '',
+      [RegisterFormKeys.Password]: '',
+      [RegisterFormKeys.ConfirmPass]: '',
+   });
 
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="maria@email.com" />
+   return (
+      <section id="register-page" className="content auth">
+         <form id="register" onSubmit={onSubmit}>
+            <div className="container">
+               <div className="brand-logo"></div>
+               <h1>Register</h1>
 
-                    <label htmlFor="pass">Password:</label>
-                    <input type="password" name="password" id="register-password" />
+               <label htmlFor="email">Email:</label>
+               <input
+                  type="email"
+                  id="email"
+                  name={RegisterFormKeys.Email}
+                  value={values[RegisterFormKeys.Email]}
+                  onChange={onChange}
+                  placeholder="maria@email.com"
+               />
 
-                    <label htmlFor="con-pass">Confirm Password:</label>
-                    <input type="password" name="confirm-password" id="confirm-password" />
+               <label htmlFor="pass">Password:</label>
+               <input
+                  type="password"
+                  name={RegisterFormKeys.Password}
+                  value={values[RegisterFormKeys.Password]}
+                  onChange={onChange}
+                  id="register-password"
+               />
 
-                    <input className="btn submit" type="submit" value="Register" />
+               <label htmlFor="con-pass">Confirm Password:</label>
+               <input
+                  type="password"
+                  name={RegisterFormKeys.ConfirmPass}
+                  value={values[RegisterFormKeys.ConfirmPass]}
+                  onChange={onChange}
+                  id="confirm-password"
+               />
 
-                    <p className="field">
-                        <span>If you already have profile click <a href="#">here</a></span>
-                    </p>
-                </div>
-            </form>
-        </section>
-    );
+               <input className="btn submit" type="submit" />
+
+               <p className="field">
+                  <span>
+                     If you already have profile click <a href="#">here</a>
+                  </span>
+               </p>
+            </div>
+         </form>
+      </section>
+   );
 }
