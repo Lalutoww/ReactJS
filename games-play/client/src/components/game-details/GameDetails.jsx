8efcb5eb11fd938023608ac7c678e5708463d802
Martin Lalutov <martinlalutov00@gmail.com>
2023-11-22 20:24:51 +0200

    [GP] add comment relations

--- games-play/client/src/components/game-details/GameDetails.jsx
+++ games-play/client/src/components/game-details/GameDetails.jsx
@@ -1,79 +1,83 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import * as gameService from '../../services/gameService';
 import * as commentService from '../../services/commentService';
+import AuthContext from '../../contexts/authContext';
 
 export default function GameDetails() {
-    const [game, setGame] = useState({});
-    const [comments, setComments] = useState([]);
-    const { gameId } = useParams();
+   const { email } = useContext(AuthContext);
+   const [game, setGame] = useState({});
+   const [comments, setComments] = useState([]);
+   const { gameId } = useParams();
 
-    useEffect(() => {
-        gameService.getOne(gameId)
-            .then(setGame);
+   useEffect(() => {
+      gameService.getOne(gameId).then(setGame);
 
-        commentService.getAll(gameId)
-            .then(setComments);
-    }, [gameId]);
+      commentService.getAll(gameId).then(setComments);
+   }, [gameId]);
 
-    const addCommentHandler = async (e) => {
-        e.preventDefault();
+   const addCommentHandler = async (e) => {
+      e.preventDefault();
 
-        const formData = new FormData(e.currentTarget);
+      const formData = new FormData(e.currentTarget);
 
-        const newComment = await commentService.create(
-            gameId,
-            formData.get('username'),
-            formData.get('comment')
-        );
+      const newComment = await commentService.create(
+         gameId,
+         formData.get('comment')
+      );
 
-        setComments(state => [...state, newComment]);
-    }
+      setComments((state) => [...state, { ...newComment, owner: { email } }]);
+   };
 
-    return (
-        <section id="game-details">
-            <h1>Game Details</h1>
-            <div className="info-section">
-                <div className="game-header">
-                    <img className="game-img" src={game.imageUrl} alt={game.title} />
-                    <h1>{game.title}</h1>
-                    <span className="levels">MaxLevel: {game.maxLevel}</span>
-                    <p className="type">{game.category}</p>
-                </div>
+   return (
+      <section id="game-details">
+         <h1>Game Details</h1>
+         <div className="info-section">
+            <div className="game-header">
+               <img className="game-img" src={game.imageUrl} alt={game.title} />
+               <h1>{game.title}</h1>
+               <span className="levels">MaxLevel: {game.maxLevel}</span>
+               <p className="type">{game.category}</p>
+            </div>
 
-                <p className="text">{game.summary}</p>
+            <p className="text">{game.summary}</p>
 
-                <div className="details-comments">
-                    <h2>Comments:</h2>
-                    <ul>
-                        {comments.map(({ _id, username, text }) => (
-                            <li key={_id} className="comment">
-                                <p>{username}: {text}</p>
-                            </li>
-                        ))}
-                    </ul>
+            <div className="details-comments">
+               <h2>Comments:</h2>
+               <ul>
+                  {comments.map(({ _id, text, owner}) => (
+                     <li key={_id} className="comment">
+                        <p>
+                           {owner.email}: {text}
+                        </p>
+                     </li>
+                  ))}
+               </ul>
 
-                    {comments.length === 0 && (
-                        <p className="no-comment">No comments.</p>
-                    )}
-                </div>
+               {comments.length === 0 && (
+                  <p className="no-comment">No comments.</p>
+               )}
+            </div>
 
-                {/* <!-- Edit/Delete buttons ( Only for creator of this game )  -->
+            {/* <!-- Edit/Delete buttons ( Only for creator of this game )  -->
                 <div className="buttons">
                     <a href="#" className="button">Edit</a>
                     <a href="#" className="button">Delete</a>
                 </div> */}
-            </div>
+         </div>
 
-            <article className="create-comment">
-                <label>Add new comment:</label>
-                <form className="form" onSubmit={addCommentHandler}>
-                    <input type="text" name="username" placeholder="username" />
-                    <textarea name="comment" placeholder="Comment......"></textarea>
-                    <input className="btn submit" type="submit" value="Add Comment" />
-                </form>
-            </article>
-        </section>
-    );
+         <article className="create-comment">
+            <label>Add new comment:</label>
+            <form className="form" onSubmit={addCommentHandler}>
+               <textarea name="comment" placeholder="Comment......"></textarea>
+               <input
+                  className="btn submit"
+                  type="submit"
+                  value="Add Comment"
+               />
+            </form>
+         </article>
+      </section>
+   );
 }
