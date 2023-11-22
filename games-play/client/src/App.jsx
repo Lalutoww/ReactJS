5128f8c5035ad84c810cfe242063a950f1674530
Martin Lalutov <martinlalutov00@gmail.com>
2023-11-22 18:01:50 +0200

    add loginSubmitHandler

--- games-play/client/src/App.jsx
+++ games-play/client/src/App.jsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Header from "./components/header/Header"
-import Home from "./components/home/Home"
+import { useState } from 'react';
+
+import Header from './components/header/Header';
+import Home from './components/home/Home';
 import GameList from './components/game-list/GameList';
 import GameCreate from './components/game-create/GameCreate';
 import Login from './components/login/Login';
@@ -9,20 +11,29 @@ import Register from './components/register/Register';
 import GameDetails from './components/game-details/GameDetails';
 
 function App() {
-    return (
-        <div id="box">
-            <Header />
+   const [auth, setAuth] = useState({});
 
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/games" element={<GameList />} />
-                <Route path="/games/create" element={<GameCreate />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/games/:gameId" element={<GameDetails /> } />
-            </Routes>
-        </div>
-    )
+   const loginSubmitHandler = (values) => {
+      //
+   };
+
+   return (
+      <div id="box">
+         <Header />
+
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/games" element={<GameList />} />
+            <Route path="/games/create" element={<GameCreate />} />
+            <Route
+               path="/login"
+               element={<Login loginSubmitHandler={loginSubmitHandler} />}
+            />
+            <Route path="/register" element={<Register />} />
+            <Route path="/games/:gameId" element={<GameDetails />} />
+         </Routes>
+      </div>
+   );
 }
 
-export default App
+export default App;
