adb92629f32a6d40079f268065ddcfc5e68cebf2
Martin Lalutov <martinlalutov00@gmail.com>
2023-11-22 18:01:36 +0200

    add LoginFormKeys

--- games-play/client/src/components/login/Login.jsx
+++ games-play/client/src/components/login/Login.jsx
@@ -1,22 +1,46 @@
-export default function Login() {
-    return (
-        <section id="login-page" className="auth">
-            <form id="login">
+import { useForm } from '../../hooks/useForm.js';
 
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Login</h1>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+const LoginFormKeys = {
+   Email: 'email',
+   Password: 'password',
+};
+export default function Login({ loginSubmitHandler }) {
+   const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
+      [LoginFormKeys.Email]: '',
+      [LoginFormKeys.Password]: '',
+   });
+   return (
+      <section id="login-page" className="auth">
+         <form id="login" onSubmit={onSubmit}>
+            <div className="container">
+               <div className="brand-logo"></div>
+               <h1>Login</h1>
+               <label htmlFor="email">Email:</label>
+               <input
+                  type="email"
+                  id="email"
+                  name={LoginFormKeys.Email}
+                  onChange={onChange}
+                  value={LoginFormKeys.Email}
+                  placeholder="Sokka@gmail.com"
+               />
 
-                    <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
-                    <input type="submit" className="btn submit" value="Login" />
-                    <p className="field">
-                        <span>If you don't have profile click <a href="#">here</a></span>
-                    </p>
-                </div>
-            </form>
-        </section>
-    );
+               <label htmlFor="login-pass">Password:</label>
+               <input
+                  type="password"
+                  id="login-password"
+                  name={LoginFormKeys.Password}
+                  onChange={onChange}
+                  value={LoginFormKeys.Password}
+               />
+               <input type="submit" className="btn submit" value="Login" />
+               <p className="field">
+                  <span>
+                     If you don't have profile click <a href="#">here</a>
+                  </span>
+               </p>
+            </div>
+         </form>
+      </section>
+   );
 }
